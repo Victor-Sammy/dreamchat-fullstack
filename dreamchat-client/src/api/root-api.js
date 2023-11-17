0e2833fc9870fb288145e5fc403c0d9f9dd5c926
Victor-Sammy <victorsammy1997@gmail.com>
2023-11-17 17:03:12 +0100

    commit comment

--- dreamchat-client/src/api/root-api.js
+++ dreamchat-client/src/api/root-api.js
@@ -1,5 +1,7 @@
 export const rootAPI =
-  'http://localhost:5000/api' || 'https://dreamchat.onrender.com/api'
+  //'http://localhost:5000/api' ||
+  'https://dreamchat.onrender.com/api'
 
 export const baseURL =
-  'http://localhost:5000' || 'https://dreamchat.onrender.com'
+  //'http://localhost:5000' ||
+  'https://dreamchat.onrender.com'
