f1cd196eeaeac643f350c770b4bd30921ccd57b1
Victor-Sammy <victorsammy1997@gmail.com>
2023-11-17 16:23:24 +0100

    added deployed server link to client

--- dreamchat-client/src/api/root-api.js
+++ dreamchat-client/src/api/root-api.js
@@ -1,3 +1,5 @@
-export const rootAPI = 'http://localhost:5000/api'
+export const rootAPI =
+  'http://localhost:5000/api' || 'https://dreamchat.onrender.com/api'
 
-export const baseURL = 'http://localhost:5000'
+export const baseURL =
+  'http://localhost:5000' || 'https://dreamchat.onrender.com'
